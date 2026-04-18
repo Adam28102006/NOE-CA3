cd4d97e10f40a1b9822ea75e064ff7e7cec7aab5
Adam28102006 <A00023435@myTUDublin.ie>
2026-04-18 14:10:04 +0100

    Update event_card.tsx

---
+++ componants/event_card.tsx
@@ -0,0 +1,22 @@
+import { formatDate } from '@/lib/utils'
+
+type Event = {
+  id: number
+  title: string
+  date: string
+  location: string
+  description: string
+}
+
+export default function EventCard({ event }: { event: Event }) {
+  return (
+    <div className="card">
+      <h3>{event.title}</h3>
+
+      <p>📅 {formatDate(event.date)}</p>
+      <p>📍 {event.location}</p>
+
+      <p>{event.description}</p>
+    </div>
+  )
+}
