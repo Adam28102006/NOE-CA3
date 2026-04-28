612442c3acf4c57222ba437798b7f5df9f1a0831
Adam28102006 <A00023435@myTUDublin.ie>
2026-04-28 22:27:09 +0100

    Fixing the page for deployment
    old code really didnt work for being deployed in netifly, or for the app directory i am using instead of the pages directory

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,53 +1,16 @@
-import events from '@/data/events.json'
-import { formatDate } from '@/lib/utils'
-import { GetServerSideProps } from 'next';
-import { getAllClusteredEvents, getSimilarEvents, Event } from '../ml/eventCluster';
+export default async function EventsPage() {
+  const events = await getAllClusteredEvents();
+  const similarToFirst = events.length
+    ? await getSimilarEvents(events[0].id)
+    : [];
 
-interface EventsPageProps {
-  events: Event[];
-  similarToFirst: Event[];
-}
-
-export default function EventsPage({ events, similarToFirst }: EventsPageProps) {
-  return (
-    <div>
-      <h1>All Events</h1>
-      <ul>
-        {events.map(e => (
-          <li key={e.id}>
-            {e.title} - Cluster {e.cluster}
-          </li>
-        ))}
-      </ul>
-
-      <h2>Events similar to first event</h2>
-      <ul>
-        {similarToFirst.map(e => (
-          <li key={e.id}>{e.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const events = getAllClusteredEvents();
-  const similarToFirst = getSimilarEvents(events[0].id);
-
-  return { props: { events, similarToFirst } };
-};
-
-
-
-export default function EventsPage() {
   return (
     <main className="page">
-     
       <div className="pageHeader">
         <h2>Campus Events</h2>
         <p>Upcoming student events and activities</p>
       </div>
-      
+
       <div className="eventGrid">
         {events.map((event) => (
           <div key={event.id} className="eventCard">
@@ -56,11 +19,18 @@ export default function EventsPage() {
               <span className="eventDate">{formatDate(event.date)}</span>
             </div>
 
-            <p className="eventLocation"> {event.location}</p>
+            <p className="eventLocation">{event.location}</p>
             <p className="eventDesc">{event.description}</p>
           </div>
         ))}
       </div>
+
+      <h2>Events similar to first event</h2>
+      <ul>
+        {similarToFirst.map((e) => (
+          <li key={e.id}>{e.title}</li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
